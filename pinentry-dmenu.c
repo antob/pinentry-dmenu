c6315b5a9cb283958965c6f8b0621a10d188c818
Tobias Lindholm <tobias.lindholm@antob.se>
2021-09-20 21:17:07 +0200

    Add WM_CLASS hint.

--- pinentry-dmenu.c
+++ pinentry-dmenu.c
@@ -276,6 +276,7 @@ setup(void) {
 	XIM xim;
 	Window w, dw, *dws;
 	XWindowAttributes wa;
+	XClassHint ch = {"dmenu", "dmenu"};
 #ifdef XINERAMA
 	XineramaScreenInfo *info;
 	Window pw;
@@ -354,6 +355,7 @@ setup(void) {
 	win = XCreateWindow(dpy, parentwin, x, y, mw, mh, 0,
 	                    CopyFromParent, CopyFromParent, CopyFromParent,
 	                    CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
+	XSetClassHint(dpy, win, &ch);
 
 	/* Open input methods */
 	xim = XOpenIM(dpy, NULL, NULL, NULL);
